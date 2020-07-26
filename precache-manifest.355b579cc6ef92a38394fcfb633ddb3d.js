859f7de866c129c9bd10bac349c65281289e2eb5
Marcin Siewodnik <m.siewodnik@gmail.com>
2020-07-26 20:15:46 +0200

    Updates

---
+++ precache-manifest.355b579cc6ef92a38394fcfb633ddb3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca26f7e57728c19faaaefa1104b5c41",
+    "url": "/react-warehouse-app/index.html"
+  },
+  {
+    "revision": "71c9e8e170159f0f64b3",
+    "url": "/react-warehouse-app/static/css/main.9da3a4df.chunk.css"
+  },
+  {
+    "revision": "e11438cafe04d1accc5e",
+    "url": "/react-warehouse-app/static/js/2.764d1180.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-warehouse-app/static/js/2.764d1180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c9e8e170159f0f64b3",
+    "url": "/react-warehouse-app/static/js/main.709ecf9a.chunk.js"
+  },
+  {
+    "revision": "3c45100e4575dd9113cf",
+    "url": "/react-warehouse-app/static/js/runtime-main.ed94704f.js"
+  }
+]);
