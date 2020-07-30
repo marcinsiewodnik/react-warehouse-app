1f1946a3095d4417e8c25f2595392b570e6f5d41
Marcin Siewodnik <m.siewodnik@gmail.com>
2020-07-30 11:02:51 +0200

    Add meta tags and add description

--- src/pages/Description.js
+++ src/pages/Description.js
@@ -10,7 +10,14 @@ const Description = () => {
 
             <div className="description">
 
-                <p>Functionality</p>
+                <p>Functionality: </p>
+
+                <p>On the product list (view mode) tab you can see all the products in the system. </p>
+                <p>On the transaction list(view mode) tab you can see all the transactions that took place. </p>
+                <p>On the add new product (change mode) tab you can add a new product to the system.</p>
+                <p>On the sell product (change mode) tab you can sell a given product. A sell transaction will be generated.</p>
+                <p>On the product list (change mode) tab you can modify existing products. Especially you can increase the quantity in stock. Then an add transaction will be generated.</p>
+
 
             </div>
 
